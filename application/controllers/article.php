b1a6cc8ad2d643aff47c6585226a38a589ced6ff
G_will <g_will@ieqi.com>
2013-05-26 19:48:20 +0800

    文章 controller

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -9,10 +9,30 @@ class Article_Controller extends Base_Controller {
 	|
 	|
 	*/
-
-	public function action_index()
+    
+    function __construct()
+    {
+        parent::__construct();
+        
+        $this->restful = true;
+        //$this->filter('before', 'auth')->only(array('index'));
+    }
+    
+	public function get_index()
 	{
-		return View::make('article.index');
+        $articles = Article::all();
+		return View::make('article.index')
+                   ->with('articles', $articles);
+	}
+    
+    public function get_detail($id, $page=1)
+	{
+        $article = Article::find($id);
+        $article_comments = ArticleComment::where_article_id($id)->get();
+		return View::make('article.detail')
+                   ->with('article', $article)
+                   ->with('page', $page)
+                   ->with('article_comments', $article_comments);
 	}
 
 }

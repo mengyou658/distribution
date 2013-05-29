3751b51e6b3f547326380bdc89d6d388bb597756
G_will <g_will@ieqi.com>
2013-05-29 17:40:05 +0800

    评论 @ 功能相关

--- public/js/main.js
+++ public/js/main.js
@@ -18,5 +18,26 @@
         
         my_editor.refreshPreview();
         MathJax.Hub.Queue(['Typeset', MathJax.Hub, wmd_preview]);
-    });            
+    });
+    
+    // for @ 
+    $('.cmt_content').each(function(){
+        var content = $(this).text().trim() + ' ';
+        var content_f = content.replace(/(@\S{4,12})\s/g, function(str){
+            var name = str.trim().slice(1);
+            //console.log(name);
+            return '<a href="/user/'+name+'">'+str.trim()+'</a> ';
+        });
+        $(this).html(content_f);
+    });
+    
+    // for test
+    $('#submit').click(function(){
+        var preview = $('#wmd-preview').html();
+        $.post('/test', {'md_input':preview}, function(result){
+            console.log('success');
+            location.reload();
+        });
+    });
+    
 })();

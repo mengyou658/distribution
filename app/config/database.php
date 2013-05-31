f6c29c770daedcdc92ab1932b48a751995723639
G_will <g_will@ieqi.com>
2013-05-31 12:51:33 +0800

    基础配置修改

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'distribution',
+			'username'  => 'distribution',
+			'password'  => 'distribution',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

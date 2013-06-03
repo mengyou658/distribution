37356cc37e0e2beaa97a869275268334e45cb4d1
G_will <g_will@ieqi.com>
2013-06-03 13:06:18 +0800

    修改 models 可绑定规则

--- app/models/NewsComment.php
+++ app/models/NewsComment.php
@@ -3,5 +3,6 @@
 class NewsComment extends Eloquent {
 
     protected $table = 'news_comments';
-
+    protected $fillable = array('news_id', 'markdown', 'content', 'author_id', 'author');
+    
 }
--- app/models/PostComment.php
+++ app/models/PostComment.php
@@ -3,5 +3,6 @@
 class PostComment extends Eloquent {
 
     protected $table = 'post_comments';
-
+    protected $fillable = array('post_id', 'markdown', 'content', 'author_id', 'author');
+    
 }

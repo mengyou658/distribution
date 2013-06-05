7720c9b2eb72938b3db14d39349217f605664b13
G_will <g_will@ieqi.com>
2013-06-05 17:05:21 +0800

    建立单页

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,6 +15,29 @@ class HomeController extends BaseController {
 		return View::make('index');
 	}
     
+    public function getAbout()
+	{
+		return View::make('home.about');
+	}
     
+    public function getContact()
+	{
+		return View::make('home.contact');
+	}
+    
+    public function getPolicy()
+	{
+		return View::make('home.policy');
+	}
+    
+    public function getHelp()
+	{
+		return View::make('home.help');
+	}
+    
+    public function getProjects()
+	{
+		return View::make('home.projects');
+	}
 
 }
---
+++ app/views/home/about.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+@section('content')
+<div class="row-fluid">
+    <div class="span3">
+    
+      <div class="well">
+        <legend>统计之都</legend>
+        <ul class="nav nav-list">
+          <!--
+          <li class="nav-header">设置</li>
+          -->
+          <li><a href="/">首页</a></li>
+          <li class="active"><a href="/about">关于我们</a></li>
+          <li><a href="/contact">联系我们</a></li>
+          <li><a href="/policy">免责声明</a></li>
+          <li><a href="/help">帮助中心</a></li>
+
+        </ul>
+      </div>
+    </div>
+    
+    <div class="span9">
+        <legend>关于我们</legend>
+
+        
+    </div>
+</div>
+@endsection

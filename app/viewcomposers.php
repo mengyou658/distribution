8fc3e0633b798382e92e443137324a5254e29512
G_will <g_will@ieqi.com>
2013-06-08 14:42:58 +0800

    view composer 测试

--- app/viewcomposers.php
+++ app/viewcomposers.php
@@ -13,3 +13,21 @@ View::composer('*', function($view)
     // 全局 view 渲染数据绑定
     //$view->with('test', 123);
 });
+
+View::composer('news.index', function($view)
+{
+    // function object_fetch($object, $attr)
+	// {
+        // $results = array_map(function($obj)use(&$attr){return $obj->$attr; }, $object);
+		// return $results;
+	// }
+    
+    // $items = $view->news->getItems();
+    // $item_ids = object_fetch($items, 'id');
+    
+    //$check = DB::table('news_digg')->whereUser_id(1)->whereIn('news_id', array(1))->first();
+    // $array = array();
+    // $array = array_add($array, $check->news_id, $check->user_id);
+    // dd($array);
+    // dd($item_ids);
+});

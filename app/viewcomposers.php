2c1bc93983bbc9d433c372463621db40e0f044f0
G_will <g_will@ieqi.com>
2013-06-16 13:14:17 +0800

    已顶的逻辑

--- app/viewcomposers.php
+++ app/viewcomposers.php
@@ -31,3 +31,36 @@ View::composer('news.index', function($view)
     // dd($array);
     // dd($item_ids);
 });
+
+View::composer('article.detail', function($view)
+{
+    $user = Auth::user();
+    
+    $comments = $view->article_comments->getCollection();
+    $comment_ids = $comments->lists('id');
+    
+    $checks = DB::table('article_comment_digg')->whereUser_id($user->id)->whereIn('article_comment_id', $comment_ids)->get();
+    
+    $digg_marks = array();
+    foreach ($checks as $check) {
+        $digg_marks[$check->article_comment_id] = $check->user_id;
+    }
+    
+    $view->with('digg_marks', $digg_marks);
+    // use this to check
+    
+    // function object_fetch($object, $attr)
+	// {
+        // $results = array_map(function($obj)use(&$attr){return $obj->$attr; }, $object);
+		// return $results;
+	// }
+    
+    // $items = $view->news->getItems();
+    // $item_ids = object_fetch($items, 'id');
+    
+    //$check = DB::table('news_digg')->whereUser_id(1)->whereIn('news_id', array(1))->first();
+    // $array = array();
+    // $array = array_add($array, $check->news_id, $check->user_id);
+    // dd($array);
+    // dd($item_ids);
+});

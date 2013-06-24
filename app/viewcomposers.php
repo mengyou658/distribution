e270388742625797586a1f3a47a90a2e567bb20e
G_will <g_will@ieqi.com>
2013-06-24 18:05:33 +0800

    修改view渲染

--- app/viewcomposers.php
+++ app/viewcomposers.php
@@ -21,7 +21,22 @@ View::composer('news.index', function($view)
         // $results = array_map(function($obj)use(&$attr){return $obj->$attr; }, $object);
 		// return $results;
 	// }
-    
+    if (Auth::check()) {
+        $user = Auth::user();
+        $items = $view->news->getCollection();
+        
+        
+        $item_ids = $items->lists('id');
+        
+        $checks = DB::table('news_digg')->whereUser_id($user->id)->whereIn('news_id', $item_ids)->get();
+        
+        $digg_marks = array();
+        foreach ($checks as $check) {
+            $digg_marks[$check->news_id] = $check->user_id;
+        }
+        
+        $view->with('digg_marks', $digg_marks);
+    }
     // $items = $view->news->getItems();
     // $item_ids = object_fetch($items, 'id');
     
@@ -34,19 +49,24 @@ View::composer('news.index', function($view)
 
 View::composer('article.detail', function($view)
 {
-    $user = Auth::user();
-    
-    $comments = $view->article_comments->getCollection();
-    $comment_ids = $comments->lists('id');
-    
-    $checks = DB::table('article_comment_digg')->whereUser_id($user->id)->whereIn('article_comment_id', $comment_ids)->get();
-    
-    $digg_marks = array();
-    foreach ($checks as $check) {
-        $digg_marks[$check->article_comment_id] = $check->user_id;
+    if (Auth::check()) {
+        $user = Auth::user();
+        
+        $comments = $view->article_comments->getCollection();
+        
+        if (!$comments->isEmpty()) {
+            $comment_ids = $comments->lists('id');
+            $checks = DB::table('article_comment_digg')->whereUser_id($user->id)->whereIn('article_comment_id', $comment_ids)->get();
+            
+        
+            $digg_marks = array();
+            foreach ($checks as $check) {
+                $digg_marks[$check->article_comment_id] = $check->user_id;
+            }
+            
+            $view->with('digg_marks', $digg_marks);
+        }
     }
-    
-    $view->with('digg_marks', $digg_marks);
     // use this to check
     
     // function object_fetch($object, $attr)

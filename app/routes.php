6be08221fad3d903db0db89c31d8d8eb1efb8a48
G_will <g_will@ieqi.com>
2014-10-19 22:25:33 +0800

    基础路由设计

--- app/routes.php
+++ app/routes.php
@@ -15,8 +15,8 @@
 */
 
 if(Config::get('app.debug')) {
-    // Event::listen('illuminate.query', function($query, $params, $time, $conn) 
-    // {    
+    // Event::listen('illuminate.query', function($query, $params, $time, $conn)
+    // {
     //    echo "<!-- debug \n";
     //    print_r(array($query, $params, $time, $conn));
     //    echo "-->\n";
@@ -51,3 +51,92 @@ if(Config::get('app.debug')) {
 */
 
 Route::get('/', 'HomeController@getIndex');
+
+// pages
+Route::get('about', 'HomeController@getAbout');
+Route::get('contact', 'HomeController@getContact');
+Route::get('policy', 'HomeController@getPolicy');
+Route::get('help', 'HomeController@getHelp');
+
+Route::get('project', 'HomeController@getProject');
+
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('user/signup', 'UserController@getSignup');
+Route::post('user/signup', 'UserController@postSignup');
+
+Route::get('user/login', 'UserController@getLogin');
+Route::post('user/login', 'UserController@postLogin');
+
+Route::get('user/logout', 'UserController@getLogout');
+
+Route::get('user/dashboard', 'UserController@getDashboard');
+
+Route::get('user/{user_id}', 'UserController@getUserDetail');
+
+Route::get('user/setting/profile', 'UserController@getSettingProfile');
+Route::post('user/setting/profile', 'UserController@postSettingProfile');
+
+Route::get('user/setting/email', 'UserController@getSettingEmail');
+Route::post('user/setting/email', 'UserController@postSettingEmail');
+
+Route::get('user/setting/avatar', 'UserController@getSettingAvatar');
+Route::post('user/setting/avatar', 'UserController@postSettingAvatar');
+
+Route::get('user/setting/password', 'UserController@getSettingPassword');
+Route::post('user/setting/password', 'UserController@postSettingPassword');
+
+/*
+|--------------------------------------------------------------------------
+| Article Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('article', 'ArticleController@getIndex');
+
+
+/*
+|--------------------------------------------------------------------------
+| News Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Question Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Discussion Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Event (Activity) Routes 
+|--------------------------------------------------------------------------
+|
+*/
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Comment Routes
+|--------------------------------------------------------------------------
+|
+*/
+

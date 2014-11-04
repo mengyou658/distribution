fa7d0adb9a725ce24b922ac638b6cc44f21e973f
G_will <g_will@ieqi.com>
2014-11-04 18:31:54 +0800

    增加 mathjax 相关内容

---
+++ app/views/utils/mathjax.blade.php
@@ -0,0 +1,13 @@
+    
+    <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({
+            tex2jax: {
+                // processClass: "mathjax",
+                // ignoreClass: "no-mathjax",
+                inlineMath: [['$','$']],
+                displayMath: [['$$','$$']]
+            }
+        });
+    </script>
+    <script type='text/javascript' src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML'></script>
+  

458726d672c97cb0b1da542d585024a4277b3af0
G_will <g_will@ieqi.com>
2014-11-04 18:32:14 +0800

    基本文章模板

---
+++ app/views/article/detail.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('content')
+<div class="row">
+    <div class="col-sm-8">
+        <div class="page-header">
+            <legend>文章</legend>
+        </div>
+
+        <div class="article-detail">
+            <h2>{{$article->title}}</h2>
+            <p>{{$article->published_at}}</p>
+            <div>
+                {{$article->content}}
+            </div>
+        </div>
+        
+        <hr>
+
+        @include('utils.comment', ['topic' => $topic, 'comments' => $comments])
+
+    </div>
+
+    <div class="col-sm-4">
+        @include('sidebar')
+    </div>
+</div>
+@stop
--- app/views/article/index.blade.php
+++ app/views/article/index.blade.php
@@ -1,5 +1,33 @@
 @extends('layout')
 
 @section('content')
-article.index
+<div class="row">
+    <div class="col-sm-8">
+        <div class="page-header">
+            <legend>文章</legend>
+        </div>
+
+        <div class="article-list">
+            @foreach($articles as $article)
+            <div class="article-item">
+                <h1><a href="{{ action('ArticleController@getDetail', $article->id) }}">{{ $article->title }}</a></h1>
+                <div>
+                    <img src="">
+                </div>
+                <div>
+                    {{$article->abstract}}
+                </div>
+                <p><a href="#">阅读全文</a> <a href="#comment">发表评论</a></p>
+            </div>
+            @endforeach
+
+            {{$articles->links()}}
+        </div>
+        
+    </div>
+
+    <div class="col-sm-4">
+        @include('sidebar')
+    </div>
+</div>
 @stop

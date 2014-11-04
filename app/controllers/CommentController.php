624db8cdf213abe22a0bb7095c675c670b11b108
G_will <g_will@ieqi.com>
2014-11-04 18:29:56 +0800

    评论相关实现

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -7,12 +7,11 @@ class CommentController extends BaseController {
     | Comment Controller
     |--------------------------------------------------------------------------
     |
-    |
     */
 
     public function __construct(){
         $this->beforeFilter('auth', array('only' => array(
-            // 'getDashboard',
+            'postIndex',
             // 'getSettingAvatar',
             // 'postSettingAvatar',
             // 'getSettingPassword',
@@ -20,8 +19,63 @@ class CommentController extends BaseController {
         )));
     }
 
-    // public function getIndex() {
-    //     return View::make('event.index');
-    // }
+    public function postIndex() {
+        //dd(Input::all());
+
+        $user = Auth::user();
+        $refer = Input::get('refer', false);
+
+        $input = [
+            'user_id' => $user->id,
+            'topic_id' => Input::get('topic_id'),
+            'content' => Input::get('content'),
+        ];
+
+        $rules = array(
+            'user_id' => 'required',
+            'topic_id' => 'required',
+            'content' => 'required',
+        );
+
+        $validator = Validator::make($input, $rules);
+        
+        if ($validator->fails()) {
+
+            if ($refer) {
+                return Redirect::to($refer)->with('comment_message', '评论发布失败');
+            }
+            else {
+                return Redirect::back()->with('comment_message', '评论发布失败');
+            }
+            
+        }
+
+        // @todo: 看看有没有更好的办法
+        $input['content'] = '<p>' . nl2br(e($input['content'])) . '</p>';
+
+        $comment = Comment::create($input);
+
+        if ($refer) {
+            return Redirect::to($refer)->with('comment_message', '评论发布成功');
+        }
+        else {
+            return Redirect::back()->with('comment_message', '评论发布成功');
+        }
+    }
+
+    public function getTopic($topicId) {
+
+        if (!Request::ajax()) {
+            App::abort(404);
+        }
+
+        $comments = Topic::find($topicId)
+                         ->comments()
+                         ->orderBy('created_at', 'desc')
+                         ->get();
+
+        // @todo: 设置 hidden 字段
+        return $comments->toJson();
+    }
 
 }

33d6464767e94813371de6296f8bc8fac4e686e1
G_will <g_will@ieqi.com>
2014-11-18 18:19:49 +0800

    侧边栏修改

--- app/views/sidebar.blade.php
+++ app/views/sidebar.blade.php
@@ -1,12 +1,53 @@
-        <div class="page-header">
-            <legend>关注我们</legend>
-        </div>
-        <div class="page-header">
-            <legend>讨论小组<small class="pull-right"><a href="#">所有小组...</a></small></legend>
-        </div>
-        <div class="page-header">
-            <legend>近期活动<small class="pull-right"><a href="#">所有活动...</a></small></legend>
-        </div>
-        <div class="page-header">
-            <legend>链接</legend>
-        </div>
+<div class="page-header">
+    <legend>关注统计之都</legend>
+</div>
+<div class="">
+    <ul>
+    <li>新浪微博<a href="http://weibo.com/cosname">@统计之都</a></li>
+    <li>人人网<a href="http://renren.com/cosname">@统计之都</a></li>
+    <li>Twitter<a href="http://twitter.com/cos_name">@cos_name</a></li>
+    </ul>
+</div>
+
+<div class="page-header">
+    <legend>微信公众平台</legend>
+</div>
+<div class="">
+<img src="/img/cos_qrcode.jpg" alt="" width="129" height="129" border="0">
+<p>微信号 CapStat</p>
+<p>我们将第一时间向您推送主站和论坛的精彩内容，以及统计之都的线下活动、竞赛、培训和会议信息。</p>
+</div>
+
+<div class="page-header">
+    <legend>讨论小组<small class="pull-right"><a href="#">所有小组...</a></small></legend>
+</div>
+
+<div class="page-header">
+    <legend>近期活动<small class="pull-right"><a href="#">所有活动...</a></small></legend>
+</div>
+
+<div class="page-header">
+    <legend>链接</legend>
+</div>
+
+<div class="">
+<ul class="">
+<li><a href="http://stat.ruc.edu.cn" title="中国人民大学统计学院网站" target="_blank">中国人民大学统计学院</a></li>
+<li><a href="http://rucdmc.net">中国人民大学数据挖掘中心</a></li>
+<li><a href="http://birc.gsm.pku.edu.cn/" target="_blank">北京大学商务智能研究中心</a></li>
+<li><a href="http://sam.cufe.edu.cn/" title="中央财经大学统计与数学学院网站" target="_blank">中央财经大学统计与数学学院</a></li>
+<li><a href="http://tjx.cueb.edu.cn/" title="首都经济贸易大学统计学院网站" target="_blank">首经贸统计学院</a></li>
+</ul>
+</div>
+
+<div class="page-header">
+    <legend>第七届R会议特别赞助商</legend>
+</div>
+
+<div class="">
+<ul class="">
+<li><a href="http://www.wquant.com/" title="微量网" target="_blank">微量网</a></li>
+<li><a href="http://supstat.com.cn/">SupStat北京数博思达数据咨询公司</a></li>
+<li><a href="http://cos.name">阿里巴巴集团</a></li>
+</ul>
+</div>

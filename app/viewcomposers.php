3e2beda7243f6ad886ee595305db3dbe864f1f03
G_will <g_will@ieqi.com>
2014-11-18 18:20:36 +0800

    当没有展示的快讯时不进行已经顶过的检查

--- app/viewcomposers.php
+++ app/viewcomposers.php
@@ -19,19 +19,22 @@ View::composer('news.index', function($view) {
     if (Auth::check()) {
         $user = Auth::user();
         $newsCollection = $view->newses->getCollection();
-        
-        $ids = $newsCollection->lists('id');
-        
-        $checks = NewsDigg::whereUser_id($user->id)
-                          ->whereIn('news_id', $ids)
-                          ->get();
-        
-        $isDuggs = [];
-        foreach ($checks as $check) {
-            $isDuggs[$check->news_id] = $check->user_id;
+
+        if(!$newsCollection->isEmpty()) {
+            $ids = $newsCollection->lists('id');
+            
+            $checks = NewsDigg::whereUser_id($user->id)
+                              ->whereIn('news_id', $ids)
+                              ->get();
+            
+            $isDuggs = [];
+            foreach ($checks as $check) {
+                $isDuggs[$check->news_id] = $check->user_id;
+            }
+            
+            $view->with('isDuggs', $isDuggs);
         }
         
-        $view->with('isDuggs', $isDuggs);
     }
 });
 

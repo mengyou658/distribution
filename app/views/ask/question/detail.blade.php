6d94cfc261edc2f28657e08c33768fb4ab3fa7dd
G_will <g_will@ieqi.com>
2014-11-23 00:22:26 +0800

    答案态度操作

--- app/views/ask/question/detail.blade.php
+++ app/views/ask/question/detail.blade.php
@@ -19,7 +19,7 @@
             </div>
         </div>
         
-        <hr>
+        <hr class="bold">
 
         <div class="page-header">
             <legend>回答</legend>
@@ -30,17 +30,21 @@
             @foreach($answers as $answer)
             <div class="answer-item row">
 
-                <div class="col-xs-1">
-                    <a href="javascript:;" class="btn btn-primary answer-approve" data-answer_id="{{$answer->id}}"><i class="fa fa-caret-up"></i></a>
+                <div class="col-xs-1  text-center answer-digg">
+                    <a href="javascript:;" class="btn btn-primary answer-approve @if(isset($attitudes[$answer->id]) && $attitudes[$answer->id] == 'approve') disabled @endif " data-answer_id="{{$answer->id}}">
+                    <i class="fa fa-caret-up"></i>
+                    </a>
 
-                    <p><span>{{$answer->vote_count}}</span></p>
+                    <p><span class="vote-count">{{$answer->vote_count}}</span></p>
 
-                    <a href="javascript:;" class="btn btn-primary answer-oppose" data-answer_id="{{$answer->id}}"><i class="fa fa-caret-down"></i></a>
+                    <a href="javascript:;" class="btn btn-primary answer-oppose @if(isset($attitudes[$answer->id]) && $attitudes[$answer->id] == 'oppose') disabled @endif" data-answer_id="{{$answer->id}}">
+                    <i class="fa fa-caret-down"></i>
+                    </a>
                 </div>
 
                 <div class="col-xs-11">
                     <div>
-                    <img src="/img/test_avatar.png" style="width:32px;"> <!-- @todo -->
+                    <img src="{{$answer->user->show_avatar}}" style="width:32px;"> <!-- @todo -->
                     <span>{{$answer->user->name}}</span> @ {{$answer->created_at}}
                     </div>
 
@@ -50,6 +54,7 @@
                     
                 </div>
             </div> 
+            <hr>
             @endforeach
 
             {{$answers->links()}}
@@ -76,17 +81,20 @@ $(function(){
         hljs.highlightBlock(block);
     });
 
-    $('.news-digg a').click(function(){
+    $('.answer-approve').click(function(){
         var _this = $(this);
-        var news_id = _this.data('news_id');
+        var answer_id = _this.data('answer_id');
         if (!_this.hasClass('disabled')) {
             _this.addClass('disabled');
-            var posting = $.post('/news/digg', { news_id:news_id });
+            var _this_oppose = _this.parent().find('.answer-oppose');
+            if (_this_oppose.hasClass('disabled')) {
+                _this_oppose.removeClass('disabled');
+            }
+            var posting = $.post('/ask/answer/approve', { answer_id:answer_id });
             posting
             .done(function(data) {
-                var digg_span = _this.parent().find('span.digg-count');
-                var digg_count = parseInt(digg_span.text())+1;
-                digg_span.text(digg_count);
+                var count_span = _this.parent().find('p span.vote-count');
+                count_span.text(data.vote_count);
             })
             .fail(function(data) {
                 // @todo: 使用 Modal 
@@ -101,6 +109,36 @@ $(function(){
             }); 
         }
     });
+
+    $('.answer-oppose').click(function(){
+        var _this = $(this);
+        var answer_id = _this.data('answer_id');
+        if (!_this.hasClass('disabled')) {
+            _this.addClass('disabled');
+            var _this_oppose = _this.parent().find('.answer-approve');
+            if (_this_oppose.hasClass('disabled')) {
+                _this_oppose.removeClass('disabled');
+            }
+            var posting = $.post('/ask/answer/oppose', { answer_id:answer_id });
+            posting
+            .done(function(data) {
+                var count_span = _this.parent().find('p span.vote-count');
+                count_span.text(data.vote_count);
+            })
+            .fail(function(data) {
+                // @todo: 使用 Modal 
+                if (data.status == 401) {
+                    alert('请登录后进行操作');
+                    // @todo: 301 to login
+                } else if (data.status == 400) {
+                    alert('已经顶过');
+                } else {
+                    alert('...发生了一些错误');
+                }
+            }); 
+        }
+    });
+
 
 });
 </script>

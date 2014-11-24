726f2ec808bbee5514934eb281f09ef484094f0a
G_will <g_will@ieqi.com>
2014-11-24 21:59:41 +0800

    修改 about

--- app/views/home/about.blade.php
+++ app/views/home/about.blade.php
@@ -56,7 +56,6 @@
 新浪微博：<a href="http://weibo.com/cosname">@统计之都</a><br>
 人人网：<a href="http://renren.com/cosname">@统计之都</a><br>
 Twitter：<a href="http://twitter.com/cos_name">@cos_name</a><br>
-QQ群：321311420<br>
 微信：CapStat</p>
 
         </div>

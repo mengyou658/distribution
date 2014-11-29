ad463b553de04af550d16d5f81cbe34292d3a33e
G_will <g_will@ieqi.com>
2014-11-29 17:39:09 +0800

    栏目修改

--- app/views/subnav.blade.php
+++ app/views/subnav.blade.php
@@ -1,8 +1,9 @@
 <div class="btn-group btn-group-justified subnav">
   <a href="/" class="btn btn-primary @if(Request::is('/')) active @endif">首页</a>
   <a href="/article" class="btn btn-primary @if(Request::is('article*')) active @endif">文章</a>
-  <a href="/news" class="btn btn-primary @if(Request::is('news*')) active @endif">聚合</a>
-  <a href="/group" class="btn btn-primary @if(Request::is('group*')) active @endif">讨论</a>
+  <a href="/news" class="btn btn-primary @if(Request::is('news*')) active @endif">快讯</a>
+  <a href="/ask" class="btn btn-primary @if(Request::is('ask*')) active @endif">问答</a>
+  <a href="/group" class="btn btn-primary @if(Request::is('group*')) active @endif">小组</a>
   <a href="/event" class="btn btn-primary @if(Request::is('event*')) active @endif">活动</a>
   <a href="/books" class="btn btn-primary @if(Request::is('books*')) active @endif">图书</a>
 </div>

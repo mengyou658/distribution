d6eed097451a8f27d3d5e358d3d629827fac7c76
G_will <g_will@ieqi.com>
2014-12-04 13:33:18 +0800

    活动参与者展示 #86

--- app/views/event/detail.blade.php
+++ app/views/event/detail.blade.php
@@ -21,10 +21,36 @@
                 <a href="{{action('ActivityController@getQuit', $activity->id)}}" class="btn btn-primary btn-wide activity-quit">退出活动</a>
                 @endif
             </p>
+
+            <div>
+                <h5>参加人</h5>
+                @foreach($activity->members as $member)
+                    <p><img src="{{$member->show_avatar}}">{{$member->name}}</p>
+                @endforeach
+            </div>
         </div>
         
         <hr>
-        @include('utils.comment')
+        {{-- @include('utils.comment') --}}
+
+        <div id="comment" class="ds-thread" data-thread-key="activity-{{$activity->id}}" data-title="{{$activity->title}}" data-url="{{action('ActivityController@getDetail', $activity->id)}}"></div>
+        <script type="text/javascript">
+        var duoshuoQuery = {
+            short_name:"cos",
+            // sso: { 
+            //     login: "http://{{cur_domain()}}/user/login/",
+            //     logout: "http://{{cur_domain()}}/user/logout/"
+            // }
+        };
+        (function() {
+            var ds = document.createElement('script');
+            ds.type = 'text/javascript';ds.async = true;
+            ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+            ds.charset = 'UTF-8';
+            (document.getElementsByTagName('head')[0] 
+             || document.getElementsByTagName('body')[0]).appendChild(ds);
+        })();
+        </script>
 
     </div>
 
